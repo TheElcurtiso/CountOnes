99a4e8631b5473f58b3127921a57ddd600cf4b8f
Elcurtiso <65019627+TheElcurtiso@users.noreply.github.com>
2022-02-04 17:16:52 +0000

    Prints Output Array

--- src/CountOnes.java
+++ src/CountOnes.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
+
 public class CountOnes {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int[][] InputArray = {
+                {1, 0, 1, 1, 0},
+                {0, 1, 1, 1, 0},
+                {1, 0, 1, 0, 1},
+                {1, 1, 0, 0, 1},
+                {0, 0, 1, 1, 0}
+        };
+        int[][] OutputArray = new int[5][5];
 
+        int ArrayLength = InputArray.length;
+        for (int i = 0; i < ArrayLength; i++)
+        {
+            for(int j = 0; j < ArrayLength; j++)
+            {
+                OutputArray[i][j] += InputArray[i][j];
+
+            }
+
+        }
+
+        System.out.println(Arrays.deepToString(OutputArray));
     }
 }
