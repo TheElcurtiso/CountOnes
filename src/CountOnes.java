ceb3a8980ac0531683322109f6d2cbe1e2aeb6fa
Elcurtiso <65019627+TheElcurtiso@users.noreply.github.com>
2022-02-05 01:55:15 +0000

    IT WORKS!

--- src/CountOnes.java
+++ src/CountOnes.java
@@ -1,28 +1,54 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CountOnes {
 
     public static void main(String[] args) {
-        int[][] InputArray = {
+        int[][] inputArray = {
                 {1, 0, 1, 1, 0},
                 {0, 1, 1, 1, 0},
                 {1, 0, 1, 0, 1},
                 {1, 1, 0, 0, 1},
                 {0, 0, 1, 1, 0}
         };
-        int[][] OutputArray = new int[5][5];
+        int[][] outputArray = new int[5][5];
 
-        int ArrayLength = InputArray.length;
-        for (int i = 0; i < ArrayLength; i++)
+        List<Integer> addArray = new ArrayList<Integer>();
+
+        int arrayLength = inputArray.length;
+        int runningTotal = 0;
+        for (int i = 0; i < arrayLength; i++)
         {
-            for(int j = 0; j < ArrayLength; j++)
-            {
-                OutputArray[i][j] += InputArray[i][j];
+            for(int j = 0; j < arrayLength; j++) {
+                //build up boxes, 1x1, 1x2, 2x1, 2x2, 3x1, 3x2, 1x3, 2x3, 3x3 etc.
+                //1x1 = 1x1
+                //1x2 = 1x1 + 1x2
+                //1x3 = 1x1 + 1x2 + 1x3
+                //2x1 = 1x1 + 2x1
+                //2x2 = 1x1 + 1x2 + 2x1
 
+                outputArray[i][j] = solve(inputArray, i, j, j, runningTotal);
             }
-
+        }
+        for (int[] array : outputArray) {
+            System.out.println(Arrays.toString(array));
         }
 
-        System.out.println(Arrays.deepToString(OutputArray));
+    }
+
+    public static int solve(int[][] inputArray, int x, int y, int originalY, int runningTotal){
+
+        if(x == 0 && y == 0) {
+            return runningTotal + inputArray[x][y];
+        }
+        else if(y > 0) {
+            return solve(inputArray, x, y - 1, originalY, runningTotal + inputArray[x][y]);
+        }
+        else if(x > 0) {
+            return solve(inputArray, x - 1, originalY,  originalY,runningTotal + inputArray[x][y]);
+        }
+
+        return 0;
     }
 }
